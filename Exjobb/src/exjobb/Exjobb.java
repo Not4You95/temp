e21108f52f4b394a36b402652f6b042ca9de14e2
Not4You95 <aikaik123>
2017-04-20 09:40:21 +0200

    :+1:

--- Exjobb/src/exjobb/Exjobb.java
+++ Exjobb/src/exjobb/Exjobb.java
@@ -10,6 +10,10 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -18,22 +22,43 @@ import javafx.stage.Stage;
  * @author jonas
  */
 public class Exjobb extends Application {
+    BorderPane root;
+    Button redButton;
     
-    @Override
+    @Override   
+    
     public void start(Stage primaryStage) {
-        Button btn = new Button();
+        Button btn = new Button(null);
+        redButton = new Button();
+        root = new BorderPane();
         btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        redButton.setText("Say 'Hello World'");
+        //ActionEvent.ACTION, new colorButton()redbutton.addEventHandler(ActionEvent.ACTION, new colorButton());
+        redButton.addEventHandler(ActionEvent.ACTION, new colorButton());
+           
+        /////////////////////////////////////////////////////////////
+        Menu menu1 = new Menu("File");
+        MenuItem SetGroupOrg = new MenuItem("SetGroup");
+        SetGroupOrg.addEventHandler(ActionEvent.ACTION, new menu1Choise());
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        MenuBar menulist = new MenuBar();
+        menulist.getMenus().addAll(menu1);
         
+        /*
+         Menu meny = new Menu("File");
+        open = new MenuItem("Open");
+        open.addEventHandler(ActionEvent.ACTION, new menuChoise());
+        save = new MenuItem("Save");
+        save.addEventHandler(ActionEvent.ACTION, new menuChoise());
+        newGame = new MenuItem("New Game");
+        newGame.addEventHandler(ActionEvent.ACTION, new menuChoise());
+        newPlayer = new MenuItem("New Player");
+        newPlayer.addEventHandler(ActionEvent.ACTION, new menuChoise());
+        meny.getItems().addAll(newGame, open, save, newPlayer);
+        */
+        /////////////////////////////////////////////////////////////
+        
+        root.setCenter(redButton);
         Scene scene = new Scene(root, 300, 250);
         
         primaryStage.setTitle("Hello World!");
@@ -47,5 +72,29 @@ public class Exjobb extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+//////////////////////////////////////////////////////////////////////////
+    private static class colorButton implements EventHandler<ActionEvent>{
+
+    
+        @Override
+        public void handle(ActionEvent event) {
+            System.out.println("Hello world!");
+        }
+        }
+////////////////////////////Menu1 action event //////////////////////////////////////////////////////
+ /**
+  * Menu1 action event
+  */
+    private static class menu1Choise implements EventHandler<ActionEvent>{
+
+       
+        @Override
+        public void handle(ActionEvent event) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+    
+
+    
     
 }
